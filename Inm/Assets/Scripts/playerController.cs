62c8a8c62b76f49aac2468ca7db785634a4dad2f
aabbas6 <36140831+aabbas6@users.noreply.github.com>
2018-12-18 00:37:25 -0500

    Decided to do Shape's as the assets
    Use Shapes that will use general movements. Eventually try to plan to create animations for them.
    Added TODOLists to

---
+++ Inm/Assets/Scripts/playerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    private float movementSpeed;
+	// Use this for initialization
+	void Start ()
+    {
+        movementSpeed = GetComponent<Stats>().getMS();	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        if(horizontalMovement > 0.5f || horizontalMovement < -0.5f)
+        {
+            transform.Translate(new Vector3(horizontalMovement * movementSpeed * Time.deltaTime, 0f, 0f));
+        }
+        if(verticalMovement > 0.5 || verticalMovement < -0.5f)
+        {
+            transform.Translate(new Vector3(0f, verticalMovement * movementSpeed * Time.deltaTime, 0f));
+        }
+
+
+	}
+}
