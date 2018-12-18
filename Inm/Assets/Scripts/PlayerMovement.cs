ddd5470c75bd77ca2cf3b2cda8657614feba3d21
aabbas6 <36140831+aabbas6@users.noreply.github.com>
2018-12-17 22:52:33 -0500

    Initial Start 12/17/2018
    Created Unity Project
    Created Player Movement

---
+++ Inm/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private float movementSpeed;
+
+    private Animator anim;
+    private bool isMoving;
+    private Vector2 lastMove;
+
+	// Use this for initialization
+	void Start ()
+    {
+        movementSpeed = GetComponent<Stats>().getMS();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        isMoving = false;
+
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+		if(horizontalMovement > 0.5f || horizontalMovement < -0.5f)
+        {
+            transform.Translate(new Vector3(horizontalMovement * movementSpeed * Time.deltaTime, 0f, 0f));
+            isMoving = true;
+            lastMove = new Vector2(horizontalMovement,0f);
+        }
+        if(verticalMovement > 0.5f || verticalMovement < -.5f)
+        {
+            transform.Translate(new Vector3(0f, verticalMovement * movementSpeed * Time.deltaTime, 0f));
+            isMoving = true;
+            lastMove = new Vector2(0f, verticalMovement);
+        }
+
+        anim.SetFloat("moveX", horizontalMovement);
+        anim.SetFloat("moveY", verticalMovement);
+        anim.SetBool("isMoving", isMoving);
+        anim.SetFloat("lastX", lastMove.x);
+        anim.SetFloat("lastY", lastMove.y);
+	}
+}
---
+++ Inm/Assets/Scripts/Stats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    //class that holds the gameobject's stats [ player + enemies ]
+    public int hp;
+    public int mp;
+
+    public int armor;
+    public int damage;
+    public int str;
+    public int inte;
+    public int dex;
+    public float speed;
+
+    public float getMS()
+    {
+        return speed;
+    }
+
+}
